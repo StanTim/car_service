f63eb2f0ef0d8f712b2eaa4ab5cf3990bd0347b7
StanTim <1694569@mail.ru>
2022-07-17 14:17:16 +0300

    add validation to name, phone fields

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
-  has_many :service
-  has_many :specialist
+  has_many :services
+  has_many :specialists
+
+  validates :client_name, presence: true
+  validates :phone, presence: true,
+            :numericality => true,
+            :length => { :minimum => 6, :maximum => 15 }
 end
