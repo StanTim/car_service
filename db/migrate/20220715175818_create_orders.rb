ac7268b73086dcecb0b1aac2c38b0f48857f7817
StanTim <1694569@mail.ru>
2022-07-17 14:13:33 +0300

    destroy refs before migration

--- db/migrate/20220715175818_create_orders.rb
+++ db/migrate/20220715175818_create_orders.rb
@@ -4,8 +4,6 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.string :client_name, null: false
       t.string :phone, null: false
       t.string :car
-      t.reference :service, null: false, foreign_key: true, index: true
-      t.reference :specialist, null: false, foreign_key: true, index: true
 
       t.timestamps
     end
