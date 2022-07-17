0ba5c263ec8f9dd17fa1de1429c7355c0bae7d2f
StanTim <1694569@mail.ru>
2022-07-17 13:49:23 +0300

    generate Orders

--- db/migrate/20220715175818_create_orders.rb
+++ db/migrate/20220715175818_create_orders.rb
@@ -1,9 +1,11 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-      t.string :client
-      t.string :phone
+      t.string :client_name, null: false
+      t.string :phone, null: false
       t.string :car
+      t.reference :service, null: false, foreign_key: true, index: true
+      t.reference :specialist, null: false, foreign_key: true, index: true
 
       t.timestamps
     end
