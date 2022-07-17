186bfbb84e6186ef9c55d39306c46d105122f676
StanTim <1694569@mail.ru>
2022-07-17 13:49:44 +0300

    generate Services

--- db/migrate/20220715182619_create_services.rb
+++ db/migrate/20220715182619_create_services.rb
@@ -2,7 +2,7 @@ class CreateServices < ActiveRecord::Migration[6.1]
   def change
     create_table :services do |t|
       t.string :name
-      t.reference :category
+      t.reference :category, null: false
 
       t.timestamps
     end
